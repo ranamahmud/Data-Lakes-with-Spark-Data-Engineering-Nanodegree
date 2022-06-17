fccdb28d516bfd328d426d6bf73611afada7e4ee
Md Rana Mahmud <ranamd0@gmail.com>
2022-06-17 14:40:52 +0600

    song_table selected

--- etl.py
+++ etl.py
@@ -22,62 +22,61 @@ def create_spark_session():
 
 
 def process_song_data(spark, input_data, output_data):
-    # get filepath to song data file
+    # TODO: get filepath to song data file
     song_data = config['S3']['song_data']
-    
-    # read song data file
+    # TODO: read song data file
     df = 
 
-    # extract columns to create songs table
-    songs_table = 
+    # TODO: extract columns to create songs table
+    songs_table = df.select("song_id","title","artist_id", "year", "duration")
     
-    # write songs table to parquet files partitioned by year and artist
+    # TODO: write songs table to parquet files partitioned by year and artist
     songs_table
 
-    # extract columns to create artists table
+    # TODO: extract columns to create artists table
     artists_table = 
     
-    # write artists table to parquet files
+    # TODO: write artists table to parquet files
     artists_table
 
 
 def process_log_data(spark, input_data, output_data):
-    # get filepath to log data file
+    # TODO: get filepath to log data file
     log_data =
 
-    # read log data file
+    # TODO: read log data file
     df = 
     
-    # filter by actions for song plays
+    # TODO: filter by actions for song plays
     df = 
 
-    # extract columns for users table    
+    # TODO: extract columns for users table    
     artists_table = 
     
-    # write users table to parquet files
+    # TODO: write users table to parquet files
     artists_table
 
-    # create timestamp column from original timestamp column
+    # TODO: create timestamp column from original timestamp column
     get_timestamp = udf()
     df = 
     
-    # create datetime column from original timestamp column
+    # TODO: create datetime column from original timestamp column
     get_datetime = udf()
     df = 
     
-    # extract columns to create time table
+    # TODO: extract columns to create time table
     time_table = 
     
-    # write time table to parquet files partitioned by year and month
+    # TODO: write time table to parquet files partitioned by year and month
     time_table
 
-    # read in song data to use for songplays table
+    # TODO: read in song data to use for songplays table
     song_df = 
 
-    # extract columns from joined song and log datasets to create songplays table 
+    # TODO: extract columns from joined song and log datasets to create songplays table 
     songplays_table = 
 
-    # write songplays table to parquet files partitioned by year and month
+    # TODO: write songplays table to parquet files partitioned by year and month
     songplays_table
 
 
